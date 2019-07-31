4f5c80a46b8add9f7a0f8f2e49e1813d7e191738
Roberto Bochet <robertobochet@gmail.com>
2019-07-31 17:23:03 +0200

    Major bugs fixed in the scrapping

--- ingress_export.js
+++ ingress_export.js
@@ -21,18 +21,21 @@
 // ==/UserScript==
 /*global $:false */
 /*global map:false */
+
 /*global L:false */
 function wrapper() {
     // in case IITC is not available yet, define the base plugin object
     if (typeof window.plugin !== "function") {
-        window.plugin = function() {};
+        window.plugin = function () {
+        };
     }
 
     // base context for plugin
-    window.plugin.portal_export = function() {};
-    var self = window.plugin.portal_export;
+    window.plugin.portalsExporter = function () {
+    };
+    var self = window.plugin.portalsExporter;
 
-    window.portals_list = {};
+    window.portals_list = new Map();
     window.portal_scraper_enabled = false;
     window.current_area_scraped = false;
 
@@ -56,7 +59,7 @@ function wrapper() {
     // return if the portal is within the drawtool objects.
     // Polygon and circles are available, and circles are implemented
     // as round polygons.
-    self.portalInForm = function(layer) {
+    self.portalInForm = function (layer) {
         if (layer instanceof L.Rectangle) {
             return true;
         }
@@ -66,7 +69,7 @@ function wrapper() {
         return false;
     };
 
-    self.portalInGeo = function(layer) {
+    self.portalInGeo = function (layer) {
         if (layer instanceof L.GeodesicPolygon) {
             return true;
         }
@@ -76,10 +79,10 @@ function wrapper() {
         return false;
     };
 
-    self.portalInDrawnItems = function(portal) {
+    self.portalInDrawnItems = function (portal) {
         var c = false;
 
-        window.plugin.drawTools.drawnItems.eachLayer(function(layer) {
+        window.plugin.drawTools.drawnItems.eachLayer(function (layer) {
             if (!(self.portalInForm(layer) || self.portalInGeo(layer))) {
                 return false;
             }
@@ -91,7 +94,7 @@ function wrapper() {
         return c;
     };
 
-    self.inBounds = function(portal) {
+    self.inBounds = function (portal) {
         if (window.plugin.drawTools && window.plugin.drawTools.drawnItems.getLayers().length) {
             return self.portalInDrawnItems(portal);
         } else {
@@ -101,13 +104,13 @@ function wrapper() {
 
     self.genStr = function genStr(title, image, lat, lng, portalGuid) {
         var href = lat + "," + lng;
-        var str= "";
+        var str = "";
         str = title;
         str = str.replace(/\"/g, "\\\"");
         str = str.replace(";", "_");
-        str = '"'+str+'"' + "," + href + "," + '"'+image+'"';
+        str = '"' + str + '"' + "," + href + "," + '"' + image + '"';
         if (window.plugin.keys && (typeof window.portals[portalGuid] !== "undefined")) {
-            var keyCount =window.plugin.keys.keys[portalGuid] || 0;
+            var keyCount = window.plugin.keys.keys[portalGuid] || 0;
             str = str + "," + keyCount;
         }
         return str;
@@ -117,23 +120,23 @@ function wrapper() {
         var lat = portal._latlng.lat,
             lng = portal._latlng.lng,
             title = portal.options.data.title || "untitled portal";
-            image = portal.options.data.image || ""
+        image = portal.options.data.image || ""
 
         return self.genStr(title, image, lat, lng, portalGuid);
     };
 
-    self.addPortalToExportList = function(portalStr, portalGuid) {
+    self.addPortalToExportList = function (portalStr, portalGuid) {
         if (typeof window.master_portal_list[portalGuid] == 'undefined') {
             window.master_portal_list[portalGuid] = portalStr;
             self.updateTotalScrapedCount()
         }
     };
 
-    self.updateTotalScrapedCount = function() {
-        $('#totalScrapedPortals').html(Object.keys(window.master_portal_list).length);
+    self.updateTotalScrapedCount = function () {
+        $('#totalScrapedPortals').text(window.portals_list.size);
     };
 
-    self.drawRectangle = function() {
+    self.drawRectangle = function () {
         var bounds = window.map.getBounds();
         var bounds = [[bounds._southWest.lat, bounds._southWest.lng], [bounds._northEast.lat, bounds._northEast.lng]];
         L.rectangle(bounds, {color: "#00ff11", weight: 1, opacity: 0.9}).addTo(window.map);
@@ -150,30 +153,66 @@ function wrapper() {
 
     };
 
+    self.isInScreen = (lat, lng) => {
+        let bounds = window.map.getBounds();
+
+        let sw = bounds.getSouthWest();
+        let ne = bounds.getNorthEast();
+
+        return (lat >= sw.lat) && (lat <= ne.lat) && (lng >= sw.lng) && (lng <= ne.lng);
+    };
+
     self.checkPortals = (portals) => {
+        let c = window.portals_list.size;
         // Parse each portal
-        portals.forEach((guid, portal) => {
+        for (let guid in portals) {
+            c++;
+            // Check if the information of portal are available
+            if (typeof window.portals[guid] === "undefined") continue;
+
             // Check if the portal is already scrapped
-            if(guid in window.portals_list) return;
+            if (window.portals_list.has(guid)) continue;
+
+            // Check if the portal is in the screen
+            if (!self.isInScreen(portals[guid]._latlng.lat, portals[guid]._latlng.lng)) continue;
 
             // Add the portal to global list
-            window.portals_list[guid] = {
-                name: portal.options.data.title || "untitled portal",
-                latitude: portal._latlng.lat,
-                longitude: portal._latlng.lng,
-                image: portal.options.data.image || ""
-            };
-        });
+            window.portals_list.set(guid, {
+                name: portals[guid].options.data.title || "untitled portal",
+                latitude: portals[guid]._latlng.lat,
+                longitude: portals[guid]._latlng.lng,
+                image: portals[guid].options.data.image || ""
+            });
+        }
+        console.debug(`Portals Exporter|Added ${window.portals_list.size - c} to portals list`);
     };
 
     self.getJSON = () => {
         let list = [];
 
-        window.portals_list.forEach((_, portal)=>{
+        // Create a list of portals
+        window.portals_list.forEach((portal) => {
             list.push(portal);
         });
 
+        // Return the JSON
         return JSON.stringify(list);
+    };
+
+    self.getCSV = () => {
+        // Create CSV header
+        let dump = "name, latitude, longitude, image\n";
+
+        // Create a list of portals
+        window.portals_list.forEach((portal) => {
+            dump += `"${portal.name.replace(/"/g, '\\\"')}",`;
+            dump += `${portal.latitude},`;
+            dump += `${portal.longitude},`;
+            dump += `"${portal.image.replace(/"/g, '\\\"')}"\n`;
+        });
+
+        // Return the csv
+        return dump
     };
 
     self.copyJSON = () => {
@@ -181,19 +220,24 @@ function wrapper() {
         navigator.clipboard.writeText(self.getJSON()).then(null);
     };
 
-    self.generateCsvData = function() {
+    self.copyCSV = () => {
+        // Copy the CSV in the user's clipboard
+        navigator.clipboard.writeText(self.getCSV()).then(null);
+    };
+
+    self.generateCsvData = function () {
         var csvData = 'Name, Latitude, Longitude, Image' + "\n";
-        $.each(window.master_portal_list, function(key, value) {
+        $.each(window.master_portal_list, function (key, value) {
             csvData += (value + "\n");
         });
 
         return csvData;
     };
 
-    self.downloadCSV = function() {
+    self.downloadCSV = function () {
         var csvData = self.generateCsvData();
         var link = document.createElement("a");
-        link.download = 'Portal_Export.csv';
+        link.download = 'portalsExporter.csv';
         link.href = "data:text/csv," + escape(csvData);
         link.click();
     }
@@ -229,24 +273,23 @@ function wrapper() {
         return dialog;
     };
 
-    self.setZoomLevel = function() {
+    self.setZoomLevel = function () {
         window.map.setZoom(15);
         $('#currentZoomLevel').html('15');
         self.updateZoomStatus();
     };
 
-    self.updateZoomStatus = function() {
+    self.updateZoomStatus = function () {
         var zoomLevel = window.map.getZoom();
         $('#currentZoomLevel').html(window.map.getZoom());
         if (zoomLevel != 15) {
             window.current_area_scraped = false;
             $('#currentZoomLevel').css('color', 'red');
             if (window.portal_scraper_enabled) $('#scraperStatus').html('Invalid Zoom Level').css('color', 'yellow');
-        }
-        else $('#currentZoomLevel').css('color', 'green');
+        } else $('#currentZoomLevel').css('color', 'green');
     };
 
-    self.updateTimer = function() {
+    self.updateTimer = function () {
         self.updateZoomStatus();
         if (window.portal_scraper_enabled) {
             if (window.map.getZoom() == 15) {
@@ -267,12 +310,12 @@ function wrapper() {
         }
     };
 
-    self.panMap = function() {
+    self.panMap = function () {
         window.map.getBounds();
         window.map.panTo({lat: 40.974379, lng: -85.624982});
     };
 
-    self.toggleStatus = function() {
+    self.toggleStatus = function () {
         if (window.portal_scraper_enabled) {
             window.portal_scraper_enabled = false;
             $('#scraperStatus').html('Stopped').css('color', 'red');
@@ -298,28 +341,26 @@ function wrapper() {
         var link = $("");
         $("#toolbox").append(link);
 
-        var csvToolbox = `
-        <div id="csvToolbox" style="position: relative;">
+        $(`
+        <div id="portalExporterToolbox" style="position: relative;">
             <p style="margin: 5px 0 5px 0; text-align: center; font-weight: bold;">Portal Exporter</p>
-            <a id="startScraper" style="position: absolute; top: 0; left: 0; margin: 0 5px 0 5px;" onclick="window.plugin.portal_export.toggleStatus();" title="Start the portal data scraper">Start</a>
-            <a id="stopScraper" style="position: absolute; top: 0; left: 0; display: none; margin: 0 5px 0 5px;" onclick="window.plugin.portal_export.toggleStatus();" title="Stop the portal data scraper">Stop</a>
+            <a id="startScraper" style="position: absolute; top: 0; left: 0; margin: 0 5px 0 5px;" onclick="window.plugin.portalsExporter.toggleStatus();" title="Start the portal data scraper">Start</a>
+            <a id="stopScraper" style="position: absolute; top: 0; left: 0; display: none; margin: 0 5px 0 5px;" onclick="window.plugin.portalsExporter.toggleStatus();" title="Stop the portal data scraper">Stop</a>
 
             <div class="zoomControlsBox" style="margin-top: 5px; padding: 5px 0 5px 5px;">
                 Current Zoom Level: <span id="currentZoomLevel">0</span>
-                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portal_export.setZoomLevel();" title="Set zoom level to enable portal data download.">Set Zoom Level</a>
+                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portalsExporter.setZoomLevel();" title="Set zoom level to enable portal data download.">Set Zoom Level</a>
             </div>
 
             <p style="margin:0 0 0 5px;">Scraper Status: <span style="color: red;" id="scraperStatus">Stopped</span></p>
             <p id="totalPortals" style="display: none; margin:0 0 0 5px;">Total Portals Scraped: <span id="totalScrapedPortals">0</span></p>
 
             <div id="csvControlsBox" style="display: none; margin-top: 5px; padding: 5px 0 5px 5px; border-top: 1px solid #20A8B1;">
-                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portal_export.copyJSON();" title="Copy JSON">Copy JSON</a>
-                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portal_export.downloadCSV();" title="Download the CSV portal data.">Download CSV</a>
+                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portalsExporter.copyJSON();" title="Copy JSON">Copy JSON</a>
+                <a style="margin: 0 5px 0 5px;" onclick="window.plugin.portalsExporter.copyCSV();" title="Copy CSV">Copy CSV</a>
             </div>
         </div>
-        `;
-
-        $(csvToolbox).insertAfter('#toolbox');
+        `).insertAfter('#toolbox');
 
         window.csvUpdateTimer = window.setInterval(self.updateTimer, 500);
 
@@ -335,8 +376,9 @@ function wrapper() {
         window.bootPlugins = [self.setup];
     }
 }
+
 // inject plugin into page
 var script = document.createElement("script");
 script.appendChild(document.createTextNode("(" + wrapper + ")();"));
 (document.body || document.head || document.documentElement)
-.appendChild(script);
+    .appendChild(script);
